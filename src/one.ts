a1382e743a8d7f5b019272af365ad5e6e7ad4bbb
akash singh <akash9936@gmail.com>
2023-07-23 23:16:54 +0000

    v2 final

---
+++ src/one.ts
@@ -0,0 +1,8 @@
+ function photoGrid(): void{
+ function closeLightbox() {
+          const overlay = document.getElementById('lightbox');
+          overlay.style.display = 'none';
+          overlay.style.alignContent = "center";
+        }
+return ;
+}
